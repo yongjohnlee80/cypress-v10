d7dd317f3f22a938c1e67a89e16c4736db8ee3c8
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-09-17 18:17:20 -0700

    Working on learning different selecting strategies

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,32 +1,134 @@
 /// <reference types="cypress" />
 
-const { describe, it } = require("node:test");
+// const { describe, it } = require("node:test");
 
 describe("Our first suite", () => {
-  beforeEach("code for every test case", () => {});
-
-  it("first test", () => {
-
-    // by Tag Name
-    cy.get('input')
-
-    // by ID
-    cy.get('#inputEmail')
-
-    // by Class name
-    cy.get('.input-full-width')
-
-    // by Attribute name
-    cy.get('[placeholder]')
-
-    // by Attribute name and value
-    cy.get('[placeholder="Email"]')
-
-    // by Class value
-    cy.get('[]')
+  beforeEach(() => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
   });
 
-  it("second test", () => {});
+  it("first test", () => {
+    // by Tag Name
+    cy.get("input");
 
-  it("third test", () => {});
+    // by ID
+    cy.get("#inputEmail");
+
+    // by Class name
+    cy.get(".input-full-width");
+
+    // by Attribute name
+    cy.get("[placeholder]");
+
+    // by Attribute name and value
+    cy.get('[placeholder="Email"]');
+
+    // by Class value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
+
+    // by Tag name and Attrivute with value
+    cy.get('input[placeholder="Email"]');
+
+    // by two different attriutes
+    cy.get('[placeholder="Email"][type="email"]');
+
+    // by tag name, attribute with value, ID and class name
+    cy.get('input[placeholder="Email"]#inputEmail.input-full-width');
+
+    // The most recommended way by Cypress
+    cy.get('[data-cy="imputEmail1"]');
+  });
+
+  it("second test", () => {
+    cy.get('[data-cy="signInButton"]');
+
+    cy.contains("Sign in");
+
+    cy.contains('[status="warning"]', "Sign in");
+
+    cy.get("#inputEmail3")
+      .parents("form")
+      .find("button")
+      .should("contain", "Sign in")
+      .parents("form")
+      .find("nb-checkbox")
+      .click();
+
+    cy.contains("nb-card", "Horizontal form").find('[type="email"]');
+  });
+
+  it("then and wrap methods", () => {
+    // cy.contains("nb-card", "Using the Grid")
+    //   .find('[for="inputEmail1"]')
+    //   .should("contain", "Email");
+    // cy.contains("nb-card", "Using the Grid")
+    //   .find('[for="inputPassword2"]')
+    //   .should("contain", "Password");
+
+    // cy.contains("nb-card", "Basic form")
+    //   .find('[for="exampleInputEmail1"]')
+    //   .should("contain", "Email address");
+
+    // cy.contains("nb-card", "Basic form")
+    //   .find('[for="exampleInputPassword1"]')
+    //   .should("contain", "Password");
+
+    cy.contains("nb-card", "Using the Grid").then((firstForm) => {
+      const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text();
+      const passwordLabelFirst = firstForm
+        .find('[for="inputPassword2"]')
+        .text();
+      expect(emailLabelFirst).to.equal("Email");
+      expect(passwordLabelFirst).to.equal("Password");
+
+      cy.contains("nb-card", "Basic form").then((secondForm) => {
+        // const emailLabelSecond = secondForm
+        //   .find('[for="exampleInputEmail1"]')
+        //   .text();
+        cy.wrap(secondForm)
+          .find('[for="exampleInputEmail1"]')
+          .should("contain", emailLabelFirst);
+
+        const passwordLabelSecond = secondForm
+          .find('[for="exampleInputPassword1"]')
+          .text();
+        expect(passwordLabelFirst).to.equal(passwordLabelSecond);
+      });
+    });
+  });
+
+  it.only("invoke command", () => {
+    // 1
+    cy.get('[for="exampleInputEmail1"]').should("contain", "Email address");
+
+    // 2
+    cy.get('[for="exampleInputEmail1"]').then((label) => {
+      expect(label.text()).to.equal("Email address");
+    });
+
+    // 3
+    cy.get('[for="exampleInputEmail1"]').then((label) => {
+      cy.wrap(label).should("contain", "Email address");
+    });
+
+    // 4
+    cy.get('[for="exampleInputEmail1"]')
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.equal("Email address");
+      });
+
+    // Checkbox
+    cy.contains("nb-card", "Basic form")
+      .find("nb-checkbox")
+      .click()
+      .find(".custom-checkbox")
+      .invoke('attr', 'class')
+    //   .then(classValue => {
+    //     expect(classValue).to.contain('checked');
+    //   })
+      .should('contain', 'checked')
+  });
 });
